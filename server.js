c974d73104cd85a9f20e096d4adfdf534da267e5
ali mohebbi <galaxy.504@gmail.com>
2019-02-08 13:50:57 +0330

    home page mostly done

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const http = require('http')
+const path = require('path')
+
+const app = express();
+const port = process.env.PORT || 3001;
+app.use(express.static(__dirname+ '/dist/personal-page'))
+
+app.get('/*',(req, res) => res.sendFile(path.join(__dirname)));
+const server = http.createServer(app);
+
+server.listen(port, ()=> console.log('Running ...'))

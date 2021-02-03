3532f9a1ca8ced39e47a7f326246750cc895ab96
ali mohebbi <ali.mohebbi@usi.ch>
2021-02-03 09:19:35 +0100

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // base_url: 'http://195.176.181.46/api/'
-  base_url: 'http://afbe30203b74.ngrok.io/api/'
+  base_url: 'http://f83aa7811131.ngrok.io/api/'
 };
